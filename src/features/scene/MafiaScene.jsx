404fd4241fb2552244105260ab076788c437228c
Gena <korarora4@gmail.com>
2026-04-16 00:46:36 +0200

    wip

--- src/features/scene/MafiaScene.jsx
+++ src/features/scene/MafiaScene.jsx
@@ -1,6 +1,6 @@
 import { memo, Suspense, useMemo, useRef } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { Cloud, Environment, OrbitControls, Stars, useFBX, useGLTF } from '@react-three/drei'
+import { Cloud, Environment, OrbitControls, Stars, useFBX, useGLTF, useTexture } from '@react-three/drei'
 import * as THREE from 'three'
 import {
   dinoModelUrl,
@@ -28,10 +28,31 @@ function easeInOutCubic(t) {
 }
 
 function ProceduralGround() {
+  const textures = useTexture({
+    map: '/textures/asphalt_pit_lane_1k/textures/asphalt_pit_lane_diff_1k.jpg',
+    normalMap: '/textures/asphalt_pit_lane_1k/textures/asphalt_pit_lane_nor_gl_1k.jpg',
+    roughnessMap: '/textures/asphalt_pit_lane_1k/textures/asphalt_pit_lane_arm_1k.jpg',
+  })
+
+  textures.map.wrapS = textures.map.wrapT = THREE.RepeatWrapping
+  textures.normalMap.wrapS = textures.normalMap.wrapT = THREE.RepeatWrapping
+  textures.roughnessMap.wrapS = textures.roughnessMap.wrapT = THREE.RepeatWrapping
+
+  const repeat = 8
+  textures.map.repeat.set(repeat, repeat)
+  textures.normalMap.repeat.set(repeat, repeat)
+  textures.roughnessMap.repeat.set(repeat, repeat)
+
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow position={[0, -0.01, 0]}>
       <circleGeometry args={[45, 64]} />
-      <meshStandardMaterial color="#2a2a2a" roughness={0.9} />
+      <meshStandardMaterial
+        map={textures.map}
+        normalMap={textures.normalMap}
+        roughnessMap={textures.roughnessMap}
+        normalScale={new THREE.Vector2(0.8, 0.8)}
+        roughness={0.9}
+      />
     </mesh>
   )
 }
